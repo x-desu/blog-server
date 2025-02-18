13f202d3c5e2478690383fb846e4a8c9722d1b1b
x-desu <amitesh5q2@gmail.com>
2025-02-19 03:42:21 +0530

    cors

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -1,60 +1,64 @@
-import Comment from '../models/comment.model.js'
-import User from '../models/user.model.js'
+import Comment from "../models/comment.model.js";
+import User from "../models/user.model.js";
 
-export const getPostComments = async(req,res,next) => {
-   const comment = await Comment.find({post:req.params.postId})
-   .populate("user","username img")
-   .sort({createdAt:-1})
+export const getPostComments = async (req, res, next) => {
+  const comment = await Comment.find({ post: req.params.postId })
+    .populate("user", "username img")
+    .sort({ createdAt: -1 });
 
-   res.status(200).json(comment)
-}
+  res.status(200).json(comment);
+};
 
-export const addComments = async(req,res,next) => {
-    const clerkUserId = req.auth.userId
-    const postId = req.params.postId
-    if(!clerkUserId){
-        return res.status(401).json("Not authenticated!")
+export const addComments = async (req, res, next) => {
+  const clerkUserId = req.auth.userId;
+  const postId = req.params.postId;
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+  const user = await User.findOne({ clerkUserId: clerkUserId });
+  console.log(req.body, clerkUserId, user);
+
+  const newComment = new Comment({
+    user: user._id,
+    post: postId,
+    desc: req.body.desc,
+    img: req.body.img,
+  });
+  try {
+    const savedComment = await newComment.save();
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+  res.status(201).json("");
+};
+
+export const deleteComments = async (req, res, next) => {
+  const clerkUserId = req.auth.userId;
+  const id = req.params.id;
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+  const role = req.auth.sessionClaims.metadata?.role || "user";
+
+  if (role === "admin") {
+    await Comment.findByIdAndDelete(req.params.id);
+    return res.status(200).json("comment deleted");
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  try {
+    const deletedComment = await Comment.findOneAndDelete({
+      _id: id,
+      user: user._id,
+    });
+    if (!deletedComment) {
+      return res.status(403).json("Delete your own comments!");
     }
-    const user = await User.findOne({clerkUserId:clerkUserId})
-   
-    const newComment = new Comment({
-        user:user._id,
-        post:postId,
-        desc:req.body.desc,
-        img:req.body.img
-    })
-    try {
-    const savedComment = await newComment.save()
-    } catch (error) {
-        console.log(error)
-       return next(error)
-    }
-    res.status(201).json("")
-}
+  } catch (error) {
+    console.log(error.message);
+  }
 
-export const deleteComments = async(req,res,next) => {
-    const clerkUserId = req.auth.userId
-    const id = req.params.id
-    if(!clerkUserId){
-        return res.status(401).json("Not authenticated!")
-    }
-    const role = req.auth.sessionClaims.metadata?.role || "user"
-
-    if(role==="admin"){
-        await Comment.findByIdAndDelete(req.params.id)
-        return res.status(200).json("comment deleted")
-    }
-
-    const user = await User.findOne({clerkUserId})
-    
-    try {
-        const deletedComment = await Comment.findOneAndDelete({_id:id,user:user._id}) 
-        if(!deletedComment){
-            return res.status(403).json("Delete your own comments!")
-        }
-    } catch (error) {
-        console.log(error.message)
-    }
-    
-    res.status(200).json("comment deleted")
-}
+  res.status(200).json("comment deleted");
+};
--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-import express from 'express'
-import userRouter from './routes/user.route.js'
-import postRouter from './routes/post.route.js'
-import commentRouter from './routes/comment.route.js'
-import webhookRouter from './routes/webhook.route.js'
-import connectDB from './lib/connectDB.js'
-import { clerkMiddleware, requireAuth } from '@clerk/express'
-import cors from 'cors'
-const app = express()
+import express from "express";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import webhookRouter from "./routes/webhook.route.js";
+import connectDB from "./lib/connectDB.js";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
+import cors from "cors";
+import path from "path";
 
-app.use(cors({
-    origin:process.env.CLIENT_URL,
-    methods:["GET","POST","DELETE","PATCH"]
-}))
-app.use(clerkMiddleware())
-app.use('/webhooks',webhookRouter)
-app.use(express.json())
+const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+  })
+);
+app.use(clerkMiddleware());
+app.use("/webhooks", webhookRouter);
+app.use(express.json());
 
-app.use('/user',userRouter)
-app.use('/post',postRouter)
-app.use('/comment',commentRouter)
+app.use("/user", userRouter);
+app.use("/post", postRouter);
+app.use("/comment", commentRouter);
 
-app.use((error,req,res,next)=>{
-    res.status(error.status||500)
-    res.json({
-        message:error.message || "Something went wrong",
-        status:error.status,
-        stack:error.stack
-    })
-})
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message || "Something went wrong",
+    status: error.status,
+    stack: error.stack,
+  });
+});
 
-app.listen(process.env.PORT,()=>{
-    connectDB()
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+// app.use(express.static(path.join(__dirname, 'dist')));
+
+// // Fallback route for all requests, serve the index.html for client-side routing
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+// });
+
+app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
